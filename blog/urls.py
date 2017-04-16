244dcb4e2d12fa50211821ca19df37cc42f3d947
Nick Weseman <trumpernickel2@yahoo.com>
2017-04-15 21:51:06 -0500

    updated .gitignore to ignore idea settings

---
+++ blog/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url
+from blog import views
+
+urlpatterns = [
+    url(r'^$', views.post_list, name='post_list'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def post_list(request, header):
+    print(header)
+    print(request)
+    print(type(header))
+    return render(request, 'blog/post_list.html', {})
+
