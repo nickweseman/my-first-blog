7a66d3d1d92f48a82b4f027fcc45af6588b15d5d
Nick Weseman <trumpernickel2@yahoo.com>
2017-04-16 09:46:06 -0500

    added post detail page and base html

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from blog.models import Post
 from django.utils import timezone
 
-def post_list(request, header):
-    print(header)
-    print(request)
-    print(type(header))
+def post_list(request):
     posts = Post.objects.filter(
         published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
