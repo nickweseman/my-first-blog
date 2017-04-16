e49c738ffd946b67464e99379f89e7b15e7305a0
Nick Weseman <trumpernickel2@yahoo.com>
2017-04-15 22:46:16 -0500

    updated template

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from blog.models import Post
+from django.utils import timezone
 
 def post_list(request, header):
     print(header)
     print(request)
     print(type(header))
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(
+        published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
