af4a7223b5dee07e2fab4fa935ccb011f9f1d40c
Nick Weseman <trumpernickel2@yahoo.com>
2017-04-16 22:40:57 -0500

    added commenting

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,15 @@
 from django import forms
 from blog.models import Post
+from blog.models import Comment
 
 class PostForm(forms.ModelForm):
 
     class Meta(object):
         model = Post
         fields = ('title', 'text',)
+
+class CommentForm(forms.ModelForm):
+
+    class Meta(object):
+        model = Comment
+        fields = ('author', 'text',)
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,12 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect
 from django.shortcuts import render
 from django.utils import timezone
-from django.contrib.auth.decorators import login_required
 
+from blog.forms import CommentForm
 from blog.forms import PostForm
+from blog.models import Comment
 from blog.models import Post
 
 
@@ -69,3 +71,38 @@ def post_publish(request, pk):
 def post_remove(request, pk):
     Post.objects.filter(pk=pk).delete()
     return redirect('post_list')
+
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+
+@login_required()
+def comment_remove(request, pk):
+    comment = Comment.objects.get(pk=pk)
+    post = comment.post
+    comment.delete()
+    return redirect('post_detail', pk=post.pk)
+
+
+@login_required()
+def comment_approve(request, pk):
+    comment = Comment.objects.get(pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+
+
